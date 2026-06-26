a72dfb20225712180adafab907f88e314ea30161
Tamago <filosofish.com@gmail.com>
2026-06-26 08:48:05 +0700

    y

--- src/config.ts
+++ src/config.ts
@@ -39,3 +39,18 @@ const config: ResolvedAstroPaperConfig = {
 };
 
 export default config;
+
+import type { GiscusProps } from "@giscus/react";
+
+export const GISCUS: GiscusProps = {
+  repo: "[ENTER REPO HERE]",
+  repoId: "[ENTER REPO ID HERE]",
+  category: "[ENTER CATEGORY NAME HERE]",
+  categoryId: "[ENTER CATEGORY ID HERE]",
+  mapping: "pathname",
+  reactionsEnabled: "0",
+  emitMetadata: "0",
+  inputPosition: "bottom",
+  lang: "en",
+  loading: "lazy",
+};
